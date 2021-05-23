8f6019812dcac606751f83fc850f83344ab7f79e
Emilia Zysk <emzysk.dev@gmail.com>
2021-05-23 19:56:03 +0900

    add seasons and episodes routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :tv_shows, only: [:index]
+  resources :tv_shows, only: [:show] do
+    resources :seasons, only: %i[index show]
+  end
+  resources :seasons, only: [] do
+    resources :episodes, only: %i[index show]
+  end
 end
