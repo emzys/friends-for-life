2afa1f89dd23a61e28dc2e889c13a7a8d9855e89
Emilia Zysk <emzysk.dev@gmail.com>
2021-05-25 11:38:07 +0900

    fixed bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # root '/'
   resources :tv_shows, only: [:show] do
     resources :seasons, only: %i[index show]
   end
